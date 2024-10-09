cef4219de84250333dc742bda68a03b02fbf88fb
woximorks <aaron.wood@moxiworks.com>
2024-10-09 13:53:08 -0700

    Adding new portfolio item, starter logo using inkscape

---
+++ db/template.sql
@@ -0,0 +1,6 @@
+-- Add a video
+-- https://gorails.com/forum/embed-youtube-video-in-rails-app
+INSERT INTO video_cards VALUES (id, 'Zrfsuu33IYI?si=ZYu_Xik8A6FGaHiv(Change this to the embed URL)', current_timestamp, current_timestamp, 'Some Description')
+
+-- Add a portfolio item
+INSERT INTO portfolio_cards VALUES (id, 'Some Title', 'Some Description', 'image_name_only(donotinclude.png)', current_timestamp, current_timestamp, 'url to project')
