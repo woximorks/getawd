c87c0e3c083c27128386e54fc84577fcbbf0764a
woximorks <aaron.wood@moxiworks.com>
2024-10-11 01:53:37 -0700

    This might make the logout button not break according to chatgpt

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import { Turbo } from "@hotwired/turbo-rails"
+import * as ActiveStorage from "@rails/activestorage"
+import Rails from "@rails/ujs"
+
+Rails.start()
+Turbo.session.drive = false
+ActiveStorage.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# devise related to delete request
+pin "@rails/ujs", to: "https://cdn.jsdelivr.net/npm/@rails/ujs@7.0.0/lib/assets/compiled/rails-ujs.js"
