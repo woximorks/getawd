293373743e2a440d3ebe142c6b440afcad149853
woximorks <aaron.wood@moxiworks.com>
2024-10-11 02:12:07 -0700

    yarn add @rails/ujs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
+import Rails from "@rails/ujs"
 import { Turbo } from "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
-import Rails from "@rails/ujs"
 
 Rails.start()
 Turbo.session.drive = false
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,7 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-# devise related to delete request
 pin "@rails/ujs", to: "https://cdn.jsdelivr.net/npm/@rails/ujs@7.0.0/lib/assets/compiled/rails-ujs.js"
