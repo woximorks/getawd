80d4e6559f55b847f4d07877a88410918ed65d94
woximorks <aaron.wood@moxiworks.com>
2024-10-12 05:36:16 -0700

    removing ujs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import Rails from "@rails/ujs"
+// import Rails from "@rails/ujs"
 import { Turbo } from "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 
-Rails.start()
+// Rails.start()
 Turbo.session.drive = false
 ActiveStorage.start()
